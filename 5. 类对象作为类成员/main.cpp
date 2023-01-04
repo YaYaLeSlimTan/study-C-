2474a94dd9950cc67eab3126f415afea11785cf7
楠 王 <3334798650@qq.com>
2023-01-04 21:20:10 +0800

    多个类的构造和析构函数按照“栈”的调用规则

--- 5. 类对象作为类成员/main.cpp
+++ 5. 类对象作为类成员/main.cpp
@@ -1,24 +1,34 @@
 #include <stdio.h>
-
+#include <iostream>
+using namespace std;
 
 class phone {
 public:
 	phone(const char* pname):m_pname(pname)
 	{
+		cout << "phone类构造函数的调用" << endl;
+	}
+	~phone() {
+		cout << "phone类析构函数的调用" << endl;
 	}
 	const char* m_pname;
 };
 
 class person {
 public:
-	person(const char* name,phone pname):m_name(name),m_pname(pname)
+	person(const char* name,phone pname):m_name(name),m_phone(pname)
 	{
+		cout << "person类构造函数的调用" << endl;
+	}
+	~person() {
+		cout << "person类析构函数的调用" << endl;
 	}
 	const char* m_name;
-	phone m_pname;
+	phone m_phone;
 };
 
 int main() {
 	person p("李华","华为mate30");
-	printf("%s拿着%s\n", p.m_name, p.m_pname);
+	//printf("%s拿着%s\n", p.m_name, p.m_phone.m_pname);
+	cout << p.m_name << "拿着： " << p.m_phone.m_pname << endl;
 }
